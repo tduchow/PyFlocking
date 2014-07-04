3b5b95f414aa8b9c4501f383859aca45131b2cf6
Teddy <tedsterDP@gmail.com>
2014-07-03 23:32:42 -0700

    A few small changes to make the loop actually work. Next up, a game object, I think, which contains the render and update functions, among others.

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,4 @@
 README.md
 
 This is a project where I attempt to use python to write a flocking simulator.
+It's largely an experiment for me in using command-line git.
--- scripts/boids.py
+++ scripts/boids.py
@@ -3,34 +3,36 @@ from pygame.locals import *
 
 
 def update():
+    #check events
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == MOUSEMOTION:
+            global mousex, mousey
             mousex, mousey = event.pos
 
 
 
 def render():
-    windowSurfaceObj.fill(whiteColor)
+    winsurf.fill(black)
 
-    pygame.draw.circle(windowSurfaceObj, redColor, (mousex, mousey), 10)
+    pygame.draw.circle(winsurf, red, (mousex, mousey), 10)
     pygame.display.update()
     
 pygame.init()
 fpsClock = pygame.time.Clock()
 size = window_width, window_height = (800, 500)
 
-windowSurfaceObj = pygame.display.set_mode(size)
+winsurf = pygame.display.set_mode(size)
 pygame.display.set_caption('Basic')
 
-redColor = pygame.Color(255, 0, 0)
-greenColor = pygame.Color(0, 255, 0)
-blueColor = pygame.Color(0, 0, 255)
-whiteColor = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+
+black = pygame.Color(0, 0, 0)
 mousex, mousey = 0, 0
 
 while True:
-    
+    update()
+    render()
     fpsClock.tick(30) # pause to run the loop at 30 frames per second
