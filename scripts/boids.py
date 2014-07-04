3c9b8bdcb3ec244af0ca7559110e9135ea62aea8
Teddy <tedsterDP@gmail.com>
2014-07-04 10:49:57 -0700

    Refactored the whole game loop to be contained in an object.

--- scripts/boids.py
+++ scripts/boids.py
@@ -1,38 +1,47 @@
 import pygame, sys
 from pygame.locals import *
 
-
-def update():
-    #check events
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == MOUSEMOTION:
-            global mousex, mousey
-            mousex, mousey = event.pos
-
-
-
-def render():
-    winsurf.fill(black)
-
-    pygame.draw.circle(winsurf, red, (mousex, mousey), 10)
-    pygame.display.update()
-    
-pygame.init()
-fpsClock = pygame.time.Clock()
-size = window_width, window_height = (800, 500)
-
-winsurf = pygame.display.set_mode(size)
-pygame.display.set_caption('Basic')
-
 red = pygame.Color(255, 0, 0)
-
 black = pygame.Color(0, 0, 0)
-mousex, mousey = 0, 0
 
-while True:
-    update()
-    render()
-    fpsClock.tick(30) # pause to run the loop at 30 frames per second
+def main():
+    pygame.init()
+    size = window_width, window_height = (800, 500)
+
+    g = Game(size, 30)
+
+    g.run()
+
+class Game:
+    def __init__(self, size, fps):
+        self.size = size
+        self.fps = fps
+        self.fpsClock = pygame.time.Clock()
+        self.drawsurf = pygame.display.set_mode(size);
+        self.mousex, self.mousey = 0, 0
+
+    def run(self):
+        while True:
+            self.checkevents()
+            self.update()
+            self.render()
+
+    def update(self):
+        pass     
+
+    def checkevents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEMOTION:
+                self.mousex, self.mousey = event.pos
+
+    def render(self):
+        ds = self.drawsurf;
+        ds.fill(black)
+
+        pygame.draw.circle(ds, red, (self.mousex, self.mousey), 10)
+        pygame.display.update()
+    
+if __name__ == '__main__': main()    
