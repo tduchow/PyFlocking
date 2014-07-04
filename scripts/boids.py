0ccaf8ac6d46cc96ff3322a7559c38f791166fb0
Teddy <tedsterDP@gmail.com>
2014-07-03 23:21:33 -0700

    Added update and render functions in an attempt to bring some sort of structure in"

--- scripts/boids.py
+++ scripts/boids.py
@@ -1,6 +1,23 @@
 import pygame, sys
 from pygame.locals import *
 
+
+def update():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEMOTION:
+            mousex, mousey = event.pos
+
+
+
+def render():
+    windowSurfaceObj.fill(whiteColor)
+
+    pygame.draw.circle(windowSurfaceObj, redColor, (mousex, mousey), 10)
+    pygame.display.update()
+    
 pygame.init()
 fpsClock = pygame.time.Clock()
 size = window_width, window_height = (800, 500)
@@ -14,36 +31,6 @@ blueColor = pygame.Color(0, 0, 255)
 whiteColor = pygame.Color(255, 255, 255)
 mousex, mousey = 0, 0
 
-r = 10
-
 while True:
-    windowSurfaceObj.fill(whiteColor)
-
-    pygame.draw.circle(windowSurfaceObj, redColor, (mousex, mousey), r)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == MOUSEMOTION:
-            mousex, mousey = event.pos
-        elif event.type == MOUSEBUTTONUP:
-            mousex, mousey = event.pos
-            if event.button in (1, 2, 3):
-                msg = 'left, middle, or right mouse click'
-            elif event.button in (4, 5):
-                if event.button == 4:
-                    r -=1
-                else:
-                    r+=1
-                msg = 'mouse scrolled up or down'
-
-        elif event.type == KEYDOWN:
-            if event.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
-                msg = 'Arrow key pressed.'
-            if event.key == K_a:
-                msg = '"A" key pressed'
-            if event.key == K_ESCAPE:
-                pygame.event.post(pygame.event.Event(QUIT))
-
-    pygame.display.update()
+    
     fpsClock.tick(30) # pause to run the loop at 30 frames per second
